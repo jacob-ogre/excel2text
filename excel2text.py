4037260cd796156112667f7b158e473cc8e4bdc8
Jacob Malcom <jacob.malcom@utexas.edu>
2013-12-31 20:42:41 -0500

    Update the license.

--- excel2text.py
+++ excel2text.py
@@ -2,11 +2,20 @@
 # excel2text.py
 # A simple program to convert Excel files to text with user-defined delimiters.
 #
-# CC0, a GPL-compliant copyright; this code is released to the public domain.
-# See <http://creativecommons.org/publicdomain/zero/1.0/>
-# 
-# Written 2013 by Jacob Malcom, jacob.malcom@utexas.edu
-#
+# Copyright (C) 2013 copyright Jacob Malcom, jacob.malcom@utexas.edu
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
@@ -15,7 +24,7 @@ import xlrd
 def main():
     """
     Convert Excel files to text.
-    
+
     USAGE:
         excel2text <infile> <delimiter>
     ARGS:
